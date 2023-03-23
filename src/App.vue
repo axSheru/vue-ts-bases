27287a59dcf6e46ced0bc4c80bc3aa18991b0b36
Alex Paredes <alexparedesr95@gmail.com>
2023-03-23 16:41:20 -0600

    FEAT: Created example of script setup using counter.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,37 @@
 <script setup lang="ts">
+  import { ref } from 'vue';
+
+  const counter = ref(0);
+
+  // Sin especificar el setup.
+  /* import { defineComponent, ref } from 'vue';
+
+  export default defineComponent({
+    setup() {
+
+      const counter = ref( 0 );
+
+      const increment = () => {
+        counter.value++;
+      };
+
+      return {
+        // Properties
+        counter,
+
+        // Methods
+        increment
+      };
+
+    }
+  }); */
+
 </script>
 
 <template>
   <div class="wrapper">
-    <h1>Hola</h1>
-    <p>Mundo</p>
+    <h1>Counter: {{ counter }}</h1>
+    <button @click="counter++">+1</button>
   </div>
 </template>
 
