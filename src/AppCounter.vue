e3224c82a43f5485dc9883a1d625a0d05121a222
Alex Paredes <alexparedesr95@gmail.com>
2023-03-24 23:01:38 -0600

    REFACTOR: Created backup for counter setup example.

---
+++ src/AppCounter.vue
@@ -0,0 +1,40 @@
+<script lang="ts" src="./App.ts">
+
+  // Sin especificar el setup.
+  /* import { defineComponent, ref } from 'vue';
+
+  export default defineComponent({
+    setup() {
+
+      const counter = ref( 0 );
+
+      const increment = () => {
+        counter.value++;
+      };
+
+      return {
+        // Properties
+        counter,
+
+        // Methods
+        increment
+      };
+
+    }
+  }); */
+
+</script>
+
+<template>
+  <div class="wrapper">
+    <h1>Counter: {{ counter }}</h1>
+    <button @click="counter++">+1</button>
+  </div>
+</template>
+
+<style scoped>
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
